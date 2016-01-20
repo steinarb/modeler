13158d800b8a9e9022bb2302bed9e5546039bab0
Steinar Bang <sb@dod.no>
2016-01-20 18:52:22 +0100

    Fixed indentation.

--- modeler.core/src/main/java/no/priv/bang/modeler/core/model/Model.java
+++ modeler.core/src/main/java/no/priv/bang/modeler/core/model/Model.java
@@ -4,28 +4,28 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Model {
-	List<Node> nodes = new ArrayList<Node>();
-	
-	public Model() {
-		for (int i = 0; i < 10; i++) {
-			Node node = new Node("Node " + i );
-			nodes.add(node);
-		}
-		 
-		//set the connection here
-		for (int i = 0; i < 10 - 1; i++) {
-			Connection connection = new Connection();
- 
-			connection.setSource(nodes.get(i));
-			connection.setTarget(nodes.get(i + 1));
- 
-			nodes.get(i).addSourceConnection(connection);
-			nodes.get(i + 1).addTargetConnection(connection);
-		}
- 	}
+    List<Node> nodes = new ArrayList<Node>();
 
-	public List<Node> getNodes() {
-		return nodes;
-	}
+    public Model() {
+        for (int i = 0; i < 10; i++) {
+            Node node = new Node("Node " + i );
+            nodes.add(node);
+        }
+
+        //set the connection here
+        for (int i = 0; i < 10 - 1; i++) {
+            Connection connection = new Connection();
+
+            connection.setSource(nodes.get(i));
+            connection.setTarget(nodes.get(i + 1));
+
+            nodes.get(i).addSourceConnection(connection);
+            nodes.get(i + 1).addTargetConnection(connection);
+        }
+    }
+
+    public List<Node> getNodes() {
+        return nodes;
+    }
 
 }
