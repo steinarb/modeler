420c699dd2f6393612b0e48af66d309c0fcecc48
Steinar Bang <sb@dod.no>
2016-01-20 18:15:05 +0100

    Fixed indentation.

--- modeler.core/src/main/java/no/priv/bang/modeler/core/OpenGefEditorAction.java
+++ modeler.core/src/main/java/no/priv/bang/modeler/core/OpenGefEditorAction.java
@@ -10,31 +10,31 @@ import org.eclipse.ui.PartInitException;
 
 public class OpenGefEditorAction implements IWorkbenchWindowActionDelegate {
 
-	private IWorkbenchWindow window;
+    private IWorkbenchWindow window;
 
-	public void run(IAction action) {
-		try {
-			System.out.println("Opening GEF editor");
-			String editorId = "no.priv.bang.modeler.core.gef.editor.Editor";
-			window.getActivePage().openEditor(new StringEditorInput("Here we can put the connection string for a Neo4J database later"), editorId);
-		} catch (PartInitException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
+    public void run(IAction action) {
+        try {
+            System.out.println("Opening GEF editor");
+            String editorId = "no.priv.bang.modeler.core.gef.editor.Editor";
+            window.getActivePage().openEditor(new StringEditorInput("Here we can put the connection string for a Neo4J database later"), editorId);
+        } catch (PartInitException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
 
-	public void selectionChanged(IAction action, ISelection selection) {
-		// TODO Auto-generated method stub
+    public void selectionChanged(IAction action, ISelection selection) {
+        // TODO Auto-generated method stub
 
-	}
+    }
 
-	public void dispose() {
-		// TODO Auto-generated method stub
+    public void dispose() {
+        // TODO Auto-generated method stub
 
-	}
+    }
 
-	public void init(IWorkbenchWindow window) {
-		this.window = window;
-	}
+    public void init(IWorkbenchWindow window) {
+        this.window = window;
+    }
 
 }
